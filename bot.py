f9be9f8b971743a53075430ab5991c29fe0b164b
Антон Левицкий <larios18000@gmail.com>
2023-07-28 17:11:17 +0400

    added db who started use bot and admins commands

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 
 import logging
 import config
+import admin
+import sqlite3
 import openai
 
 
@@ -11,6 +13,8 @@ from aiogram.utils import executor
 
 openai.api_key = config.OPENAI_API_KEY
 token = config.TOKEN
+con = sqlite3.connect('db.sqlite', check_same_thread=False)
+cur = con.cursor()
 
 
 logging.basicConfig(level=logging.INFO)
@@ -24,11 +28,47 @@ def update(messages, role, content):
 
 def main():
     print('[+] ChatGPT startet like main program')
+    cur.executescript('''
+    CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    user_name TEXT,
+    user_id TEXT
+    );
+    ''')
+    con.commit()
     executor.start_polling(dp, skip_updates=True)
+
+@dp.message_handler(commands='check')
+async def check_db(message: types.Message):
+
+    id = message.from_user.id
+    approved = int(admin.id['admin_id'])
+
+    cur.execute('''
+    SELECT *
+    FROM users
+    ''')
+    con.commit()
+
+    if id == approved:
+        for result in cur:
+            await message.answer(result)
+
+    else:
+        await message.answer('Слишком мало прав...')
 
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
+    user_name = message.from_user.first_name
+    user_id = message.from_user.id
+
+    cur.execute(f'''
+    INSERT INTO users (user_name, user_id)
+    VALUES ("{user_name}", "{user_id}");
+    ''')
+    con.commit()
+
     await message.answer(f'Привет, {message.from_user.first_name}! Я бот Синди, в которого интегрировали ChatGPT. Какой у тебя вопрос?')
 
 @dp.message_handler(commands=['developer'])
